895a51446e6dc0fd8e2d401ef7a94c6184be5745
Marnick Goossens <marnick.goossens.04@gmail.com>
2022-10-01 18:11:41 +0200

    main program

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from serial import Serial
+from re import match
 from phue import Bridge
 
-ip_hue = "192.168.0.223"
-
 ser = Serial(port="/dev/ttyUSB0", baudrate=115200)
-b = Bridge(ip_hue)
-b.connect()
+regularExpression = "b'(1-0:\d.7.0)\((\d{2}.\d{3}).*'"
 
-id_sc1 = b.get_light("sc1")
+# ip_hue = "192.168.0.223"
+# b = Bridge(ip_hue)
+# b.connect()
+
+codes = ["1-0:1.7.0", "1-0:2.7.0"]
+dictionary = {}
+
+
+def plug(dictionary):
+    verbruik = dictionary["1-0:1.7.0"] - dictionary["1-0:2.7.0"]
+    print(verbruik)
+
 
 while True:
     line = str(ser.readline())
     m = match(regularExpression, line)
     if m:
-        pass
+        code = m.group(1)
+        waarde = m.group(2)
+        if code in code_verbruik:
+            dictionary[code] = waarde
 
     if line.startswith("b'!"):
+        plug(dictionary)
         continue
